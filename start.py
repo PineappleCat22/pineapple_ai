991d61aedba4011a1d26ee46b01fce9b9872a9b9
PineappleCat22 <waylonmonroe418@gmail.com>
2024-06-22 21:22:38 -0700

    desktop start!

--- start.py
+++ start.py
@@ -2,23 +2,28 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 
-documents = SimpleDirectoryReader("data").load_data()
+print("Initializing Pineapple AI...")
 
 # bge-base embedding model
+print("Starting embedding model: BGE-BASE-EN-v1.5")
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 #apparently an embedding model is required. who knew!
 
 # ollama
-Settings.llm = Ollama(model="dave4", request_timeout=360.0)
+print("Starting base model: ###PUT MODEL NAME HERE, DUDE###")
+Settings.llm = Ollama(model="llama3", request_timeout=360.0)
 
+print("Linking data documents...")
+documents = SimpleDirectoryReader("data").load_data()
 index = VectorStoreIndex.from_documents(
     documents,
 )
 
+print("Starting the query engine...")
 query_engine = index.as_query_engine()
 while True:
-	response = query_engine.query(input("Enter a prompt:\n"))
-	print(response)
+    response = query_engine.query(input("Enter a prompt:\n"))
+    print(response)
 
 # hey so idk how to access cloned models so this just uses ollama.
 # i need to figure out how to use modelfiles which point to the model location, i guess.
@@ -49,3 +54,9 @@ while True:
 # addendum to the first three lines of notes:
 # I FIGURED IT OOOOOOOOOOOOOOOOOOUT
 # its pointless but for the sake of portability i will create the modelfiles for both bge and ll2-u, and leave them in.
+
+# yeah i didnt do that.
+# hey buddy you're probably wondering what that venv is doing there. the interpretation running on my laptop needs it for some damn reason. god this is so janky but if it works it works.
+# pep8 is a fucking bitch.
+# intellij is a fucking bitch.
+# STOP POLICING MY LANGUAGE
