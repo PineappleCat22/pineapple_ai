6b21ecc2c682b8d9be047366f99ff6342480c49e
PineappleCat22 <waylonmonroe418@gmail.com>
2024-06-29 00:35:28 -0700

    bot is kinda ready to go? can start testing autostart on the server if i want.

---
+++ start.py
@@ -0,0 +1,23 @@
+import discord
+
+with open('botToken') as f:
+    TOKEN = f.readline() #to keep my token out of malicious hands.
+
+intents = discord.Intents.default()
+intents.message_content = True #only good intentions
+
+#idk how to read documentation!!!! try to do the addition thing lmao.
+
+class MyClient(discord.Client): #hey uh what is this line
+    async def on_ready(self):
+        print(f'{self.user} ready to fuck around and find out yeehaw')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return  # Ignore messages from the bot itself
+
+        if self.user.mentioned_in(message):
+            await message.channel.send(f'hi {message.author.mention}')
+
+client = MyClient(intents=intents) #parameters??? how the fuck does this work
+client.run(TOKEN)
