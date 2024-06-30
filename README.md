59a0c453646b3761f5c5cd689fc3e75ad76abb1d
PineappleCat22 <waylonmonroe418@gmail.com>
2024-06-29 17:50:37 -0700

    gday sir or madam

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-hello guys welcome to my youtube channel. today we are installing the pineapple ai
-# installation guide
-### install the python and dependencies
-```
-sudo apt install python3 pip
-pip install llama-index-llms-ollama
-pip install llama-index-embeddings-huggingface
-```
-if the pip commands dont work you need to build a virtual environment, you can learn how to do that here: https://askubuntu.com/questions/1465218/pip-error-on-ubuntu-externally-managed-environment-%C3%97-this-environment-is-extern
-
-# TODO: ADD MORE INSTRUCTIONS
-# TODO: THIS WHOLE THING IS INVALID!
+haha uh todo make this better
--- start.py
+++ start.py
@@ -8,7 +8,7 @@ intents.message_content = True #only good intentions
 
 #idk how to read documentation!!!! try to do the addition thing lmao.
 
-class MyClient(discord.Client): #hey uh what is this line
+class MyClient(discord.Client):
     async def on_ready(self):
         print(f'{self.user} ready to fuck around and find out yeehaw')
 
@@ -17,7 +17,11 @@ class MyClient(discord.Client): #hey uh what is this line
             return  # Ignore messages from the bot itself
 
         if self.user.mentioned_in(message):
-            await message.channel.send(f'hi {message.author.mention}')
+            await message.channel.send(message.content)
+            print(message.content)
 
-client = MyClient(intents=intents) #parameters??? how the fuck does this work
+
+client = MyClient(intents=intents) #how the fuck does this work
+client.status = discord.Status.idle
+client.activity = discord.Activity(name="drinking jesus juice", type=discord.ActivityType.custom, test="balls balls nuts balls")
 client.run(TOKEN)
