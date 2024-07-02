e23d75b2aed0f9d79c9b07749afa4fd1aa34f82a
PineappleCat22 <waylonmonroe418@gmail.com>
2024-07-01 19:59:23 -0700

    the bot can now handle more errors better + easier parse messages. time to test ram

--- start.py
+++ start.py
@@ -1,6 +1,8 @@
 import discord
 import runModel as model
 import tokenizer
+import datetime as time
+import traceback
 
 tkn = tokenizer.Tokenizer() #hey i dont know why this is necessary
 with open('botToken') as f:
@@ -11,8 +13,6 @@ testmode = 1
 
 intents = discord.Intents.default()
 intents.message_content = True #only good intentions
-
-#idk how to read documentation!!!! try to do the addition thing lmao.
 
 class MyClient(discord.Client):
     try:
@@ -25,6 +25,19 @@ class MyClient(discord.Client):
         print(e)
 
     async def on_message(self, message):
+        async def sendMsg(*msgs):
+            for msg in msgs:
+                try:
+                    if len(msg) > 2000:
+                        await message.channel.send(f"aw fuck something went wrong while sending a message.\n<@189034549789851648> HEY SHITHEAD SOMETHING IS FUCKED.\nmessage send error: message longer than 2000 chars\ntime: {time.datetime.now()}")
+                        print("======================================== THIS IS WHERE THE MESSAGE SEND ERROR OCCURRED =========================")
+                        print(f"message: {msg}\n length: {len(msg)}\n time: {time.datetime.now()}")
+                    else:
+                        await message.channel.send(msg)
+                except Exception as e2:
+                    print(f"SOMETHING EVEN WORSE HAPPENED WHILE SENDING A MESSAGE.\n{traceback.format_exception(e2)}")
+
+
         if message.author == self.user:
             return  # Ignore messages from the bot itself
 
@@ -33,25 +46,30 @@ class MyClient(discord.Client):
                 try:
                     messageStr = message.content.replace("<@437414611369721856>", f"{message.author.name} says:")
                     messageEncoded = tkn.encode(s=messageStr, eos=False, bos=False)
-                    await message.channel.send(messageStr)
-                    await message.channel.send(f"tokens: {messageEncoded}")
-                    await message.channel.send(f"token length: {len(messageEncoded)}")
+                    await sendMsg(
+                        messageStr,
+                        f"tokens: {messageEncoded}",
+                        f"token length: {len(messageEncoded)}")
                 except Exception as e:
-                    await message.channel.send("Something went horribly wrong while parsing a message from a user.")
-                    await message.channel.send(e)
+                    await sendMsg(
+                        "aw fuck. message parse error.",
+                        e)
             elif testmode == 0:
                 try:
                     messageStr = message.content.replace("<@437414611369721856>", f"{message.author.name} says:")
-                    await message.channel.send(model.query(messageStr)) #parse this into the ai and print its response.
+                    await sendMsg(model.query(messageStr)) #todo: improve this later
                 except Exception as e:
-                    await message.channel.send("Something went horribly wrong while trying to send something to the AI.")
-                    await message.channel.send(e)
+                    await sendMsg(
+                        "aw fuck. ai query error.",
+                        traceback.format_exception(e))
             else:
-                await message.channel.send("testmode error. what the fuck is going on. im gonna kill myself.")
-                await message.channel.send(testmode)
+                await sendMsg(
+                    "testmode error. what the fuck is going on. im gonna kill myself.",
+                    testmode)
                 quit() #this throws a lot of errors. but it accomplishes what it needs to.
 
 #i dont know why all the awaits are here, but it fucking explodes if they arent there.
+#this code is so bad it would probably be a severe liability if it didnt run a silly little bot.
 
 client = MyClient(intents=intents) #how the fuck does this work
 client.status = discord.Status.idle #cool flavor stuff
