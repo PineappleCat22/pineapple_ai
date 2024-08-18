b3e19240b51e9dcdfac9f8871c9a80406d6d5d49
PineappleCat22 <waylonmonroe418@gmail.com>
2024-08-17 22:15:54 -0700

    rough draft of delay fix code. its bad and will probably hold up the event loop

--- start.py
+++ start.py
@@ -28,6 +28,9 @@ class MyClient(discord.Client):
             loop = asyncio.get_event_loop()
             status_message = await loop.run_in_executor(executor, query_ai_for_status)
             await self.change_presence(status=discord.Status.idle, activity=discord.CustomActivity(status_message))
+            while True: # bad
+                time.sleep(3600)
+                await self.change_presence(status=discord.Status.idle, activity=discord.CustomActivity(status_message))
 
             print(f'{self.user} initialized successfully')
             print("test mode:", testmode)
